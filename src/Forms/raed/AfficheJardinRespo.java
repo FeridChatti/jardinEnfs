14505641f3ac8ff6bd88677e100d14808b641a49
raed mejri <raed.mejri1@esprit.tn>
2020-05-11 02:12:38 +0100

    done!!

--- src/Forms/raed/AfficheJardinRespo.java
+++ src/Forms/raed/AfficheJardinRespo.java
@@ -3,6 +3,8 @@ package Forms.raed;
 import Entities.Enfant;
 import Entities.Jardin;
 import Entities.User;
+import Forms.Accueils.AccueilParent;
+import Forms.Enfants.ConsulterEnfant;
 import Forms.Enfants.ModifierEnfant;
 import Services.EnfantService;
 import Services.JardinService;
@@ -25,8 +27,9 @@ import java.util.Map;
 
 public class AfficheJardinRespo  extends Form {
     String enl="";
-    public AfficheJardinRespo  (Form prev){
-        Form th=this;
+    int count=0;
+    public AfficheJardinRespo  (Form prev ,String id) throws ParseException {
+        Form ts=this;
 
         getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK, e->prev.showBack());
         setTitle("Affiche Jardin Responsable");
@@ -63,11 +66,73 @@ Jardin j= UserService.getInstance().getJardin(MyApplication.authenticated.getId(
         Label tari = new Label("Tarif");
         TextField rse = new TextField((int) j.getTarif());
         rse.setEnabled(false);
+
+        Button md=new Button("Modifier");
+
         add(nom);
         add(t);
+        addAll(add,re,desc,ree,nute,reee,tari,rse,md);
+ts.show();
 
-        addAll(add,re,desc,ree,nute,reee,tari,rse);
 
+        md.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+
+                md.setText("valider");
+                nom.setEnabled(true);
+                add.setEnabled(true);
+                desc.setEnabled(true);
+                nute.setEnabled(true);
+                tari.setEnabled(true);
+                count=count+1;
+                if (count==1){
+                    md.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent evt) {
+
+                            Dialog.show("Confirmation","ete vous sur de modifier votre jardin ? ","Oui","Non");
+
+                            int idenf=Integer.valueOf(id);
+                            if((reee.getText().length()==0)||(t.getText().length()==0)||(re.getText().length()==0)||||(ree.getText().length()==0)){
+                                Dialog.show("Erreur","Veuillez indiquez les champs",new Command("OK"));
+                            }
+
+                            else {
+                                if (t.getText().matches("[a-zA-Z]*") && re.getText().matches("[a-zA-Z]*") && ree.getText().matches("[a-zA-Z]*")) {
+                                    if (reee.getText().matches("[a-zA-Z]*")) {
+                                        Dialog.show("Erreur", "Num tel non valide", new Command("OK"));
+                                    }
+                                    if(rse.getText().matches("[a-zA-Z]*"))
+                                    {
+                                        Dialog.show("Erreur", "Tarif non valide", new Command("OK"));
+                                    }
+
+                                    else{
+
+                                        Jardin je = new Jardin( idenf ,t.getText(), re.getText(), ree.getText(),reee.getText(),rse.getText());
+
+
+
+                                        boolean j = JardinService.getInstance().ModifierJardin(je);
+                                        if (j) {
+                                            Dialog.show("Succés", "Enfant modifié avec succés", "Oui", null);
+                                            new ConsulterEnfant(new AccueilParent()).show();
+                                        } else {
+                                            Dialog.show("Erreur", "Erreur", "Oui", null);
+                                        }
+
+
+                                    }
+
+
+                                }}}
+                    });
+
+                }
+
+            }
+        });
 
 
 
