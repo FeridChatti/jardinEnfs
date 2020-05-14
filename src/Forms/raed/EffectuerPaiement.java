d7bed69f361dc48def43aa54c119477fdcb48a50
raed mejri <raed.mejri1@esprit.tn>
2020-05-14 22:39:25 +0100

    done

--- src/Forms/raed/EffectuerPaiement.java
+++ src/Forms/raed/EffectuerPaiement.java
@@ -1,6 +1,7 @@
 package Forms.raed;
 
 import Entities.Jardin;
+import Entities.Paiement;
 import Services.UserService;
 import com.codename1.ui.*;
 import com.codename1.ui.layouts.BoxLayout;
@@ -25,5 +26,32 @@ public class EffectuerPaiement extends Form {
 
         Image icon2 = URLImage.createToStorage(placeholder, "icon2", "http://www.vippng.com/png/detail/35-352335_baby-boy-icon-png-icone-enfant-png.png");
         Jardin j= UserService.getInstance().getJardin(MyApplication.authenticated.getId()+"");
+        setLayout(BoxLayout.y());
+        Label nom = new Label("Nom:");
+        TextField t = new TextField(j.getName());
+        t.setEnabled(false);
+        Label add = new Label("Adresse");
+        TextField re = new TextField(j.getAdresse());
+        re.setEnabled(false);
+
+
+
+        Label desc = new Label("Description");
+        TextField ree = new TextField(j.getDescription());
+        ree.setEnabled(false);
+
+
+
+        Label nute = new Label("Numero Telephone ");
+        TextField reee = new TextField(j.getNumtel());
+        reee.setEnabled(false);
+
+        Label tari = new Label("Tarif");
+        TextField rse = new TextField(String.valueOf(j.getTarif()));
+        rse.setEnabled(false);
+        Paiement paiement=new Paiement();
+        Label Mont=new Label("montant a payer");
+        TextField rr= new TextField(250);
+
     }
 }
