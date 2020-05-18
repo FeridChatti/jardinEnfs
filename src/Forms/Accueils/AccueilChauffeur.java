6ff9f325e3a1a3976dc70052fc6819a6ac1e396c
karim <karim-nar@live.fr>
2020-05-18 07:54:17 +0100

    final

--- src/Forms/Accueils/AccueilChauffeur.java
+++ src/Forms/Accueils/AccueilChauffeur.java
@@ -13,8 +13,10 @@ import Forms.User.SignIn;
 import Forms.raed.AfficheJardinRespo;
 import Forms.raed.EffectuerPaiement;
 import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SpanLabel;
 import com.codename1.l10n.ParseException;
 import com.codename1.ui.*;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.layouts.LayeredLayout;
@@ -32,14 +34,51 @@ public class AccueilChauffeur extends Form {
     {fo=this;
         setLayout(BoxLayout.y());
 
-        Button cs=new Button("Voir mes trajets");
-        cs.addActionListener(e-> {
-            try {
-                new ChauffeurTrajets(fo);
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        });
+        Container form=new Container();
+
+        fo = this;
+        setLayout(BoxLayout.y());
+
+        // getToolbar().hideToolbar();
+
+        setTitle("Espace Parent");
+
+
+        getContentPane().setScrollVisible(false);
+
+        Tabs swipe = new Tabs();
+
+        Label spacer1 = new Label();
+        Label spacer2 = new Label();
+        addTab(swipe, res.getImage("swipe menu.jpg"), spacer1, "  ", "", " ");
+        swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+
+        form.add(swipe);
+
+
+        Label schoolbellabel=new Label("\nKinderG Introduction","schoolbell");
+        form.add(schoolbellabel);
+
+
+        SpanLabel titlelavel=new SpanLabel();
+        titlelavel.setText("Welcome to Kindergarten's Guides");
+        titlelavel.setTextUIID("TitleLabel");
+        form.add(titlelavel);
+
+        SpanLabel contentlbl=new SpanLabel();
+        contentlbl.setText("KinderG vous permet de trouver le jardin ideal pour votre enfant.\n" +
+                "trouver le jardin d'enfants le plus proche depend a vos besoins sans aucun effort.");
+        contentlbl.setTextUIID("txtlbl");
+        form. add(contentlbl);
+
+
+        form.setUIID("BackgroundForm");
+        form.setPreferredH(getPreferredH());
+        form.setPreferredW(getPreferredW());
+
+        add(form);
 
 
      /*   Button ch=new Button("Consulter Profil");
@@ -108,4 +147,50 @@ public class AccueilChauffeur extends Form {
 
     }
 
+    private void addTab(Tabs swipe, Image img, Label spacer, String likesStr, String commentsStr, String text) {
+
+
+
+        int size = Math.min(Display.getInstance().getDisplayWidth(), Display.getInstance().getDisplayHeight());
+
+        if(img.getHeight() < size) {
+            img = img.scaledHeight(size);
+        }
+
+        Label likes = new Label(likesStr);
+        Style heartStyle = new Style(likes.getUnselectedStyle());
+        heartStyle.setFgColor(0xff2d55);
+        FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, heartStyle);
+        likes.setIcon(heartImage);
+        likes.setTextPosition(RIGHT);
+
+        Label comments = new Label(commentsStr);
+        FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
+        if(img.getHeight() > Display.getInstance().getDisplayHeight() / 2) {
+            img = img.scaledHeight(Display.getInstance().getDisplayHeight() / 2);
+        }
+        ScaleImageLabel image = new ScaleImageLabel(img);
+        image.setUIID("Container");
+        image.setBackgroundType(Style.BACKGROUND_IMAGE_SCALED_FILL);
+        Label overlay = new Label(" ", "ImageOverlay");
+
+        Container page1 =
+                LayeredLayout.encloseIn(
+                        image,
+                        overlay,
+                        BorderLayout.south(
+                                BoxLayout.encloseY(
+                                        new SpanLabel(text, "LargeWhiteText"),
+
+                                        spacer
+                                )
+                        )
+                );
+
+        swipe.addTab("", page1);
+
+
+
+
+    }
 }
--- src/Forms/Accueils/AccueilTuteur.java
+++ src/Forms/Accueils/AccueilTuteur.java
@@ -8,7 +8,9 @@ import Forms.Sami.ProfilChauffeur;
 import Forms.User.Login;
 import Forms.User.SignIn;
 import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SpanLabel;
 import com.codename1.ui.*;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.layouts.LayeredLayout;
@@ -24,6 +26,65 @@ public class AccueilTuteur extends Form {
     public Resources res= MyApplication.theme;
     public AccueilTuteur(){
         fo = this;
+
+
+        setLayout(BoxLayout.y());
+
+        Container form=new Container();
+
+        fo = this;
+        setLayout(BoxLayout.y());
+
+        // getToolbar().hideToolbar();
+
+        setTitle("Espace Parent");
+
+
+        getContentPane().setScrollVisible(false);
+
+        Tabs swipe = new Tabs();
+
+        Label spacer1 = new Label();
+        Label spacer2 = new Label();
+        addTab(swipe, res.getImage("swipe menu.jpg"), spacer1, "  ", "", " ");
+        swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+
+        form.add(swipe);
+
+
+        Label schoolbellabel=new Label("\nKinderG Introduction","schoolbell");
+        form.add(schoolbellabel);
+
+
+        SpanLabel titlelavel=new SpanLabel();
+        titlelavel.setText("Welcome to Kindergarten's Guides");
+        titlelavel.setTextUIID("TitleLabel");
+        form.add(titlelavel);
+
+        SpanLabel contentlbl=new SpanLabel();
+        contentlbl.setText("KinderG vous permet de trouver le jardin ideal pour votre enfant.\n" +
+                "trouver le jardin d'enfants le plus proche depend a vos besoins sans aucun effort.");
+        contentlbl.setTextUIID("txtlbl");
+        form. add(contentlbl);
+
+
+        form.setUIID("BackgroundForm");
+        form.setPreferredH(getPreferredH());
+        form.setPreferredW(getPreferredW());
+
+        add(form);
+
+
+
+
+
+
+
+
+
+
        /* setLayout(BoxLayout.y());
         Button btnmyrem = new Button("remarques des enfants");
         btnmyrem.addActionListener(s -> new TuteurRemarques(fo) );
@@ -72,6 +133,54 @@ public class AccueilTuteur extends Form {
 
 
         tb.addMaterialCommandToSideMenu("Se déconnecter", FontImage.MATERIAL_LOGOUT, s ->new SignIn(MyApplication.theme).show());
+
+
+    }
+
+
+    private void addTab(Tabs swipe, Image img, Label spacer, String likesStr, String commentsStr, String text) {
+
+
+
+        int size = Math.min(Display.getInstance().getDisplayWidth(), Display.getInstance().getDisplayHeight());
+
+        if(img.getHeight() < size) {
+            img = img.scaledHeight(size);
+        }
+
+        Label likes = new Label(likesStr);
+        Style heartStyle = new Style(likes.getUnselectedStyle());
+        heartStyle.setFgColor(0xff2d55);
+        FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, heartStyle);
+        likes.setIcon(heartImage);
+        likes.setTextPosition(RIGHT);
+
+        Label comments = new Label(commentsStr);
+        FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
+        if(img.getHeight() > Display.getInstance().getDisplayHeight() / 2) {
+            img = img.scaledHeight(Display.getInstance().getDisplayHeight() / 2);
+        }
+        ScaleImageLabel image = new ScaleImageLabel(img);
+        image.setUIID("Container");
+        image.setBackgroundType(Style.BACKGROUND_IMAGE_SCALED_FILL);
+        Label overlay = new Label(" ", "ImageOverlay");
+
+        Container page1 =
+                LayeredLayout.encloseIn(
+                        image,
+                        overlay,
+                        BorderLayout.south(
+                                BoxLayout.encloseY(
+                                        new SpanLabel(text, "LargeWhiteText"),
+
+                                        spacer
+                                )
+                        )
+                );
+
+        swipe.addTab("", page1);
+
+
 
 
     }
